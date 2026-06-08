e12cb44a2b73b79b6fc3131db3021e59c2788779
IGOR-GAB30 <igor_paradella@estudante.sesisenai.org.br>
2026-06-07 23:26:27 -0300

    doc - adicionei comentários de funcionalidades na página pertencente ao infra/redes (script.sql).

--- infra/redes/script.sql
+++ infra/redes/script.sql
@@ -1,13 +1,15 @@
 CREATE DATABASE sistema_simples;
+-- Cria o banco de dados
 
 USE sistema_simples;
+-- Seleciona o banco
 
 CREATE TABLE usuario (
     id INT AUTO_INCREMENT PRIMARY KEY;
     usuario VARCHAR(255) NOT NULL;
     senha VARCHAR(255) NOT NULL;
 );
+-- Cria a tabela de usuários
 
 INSERT INTO usuario (usuario, senha) VALUES ('admin','1234');
-
- 
+-- Insere um usuário padrão
