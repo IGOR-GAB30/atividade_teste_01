2588a0bd204c1ca7e523802167905457a77b804f
IGOR-GAB30 <igor_paradella@estudante.sesisenai.org.br>
2026-06-07 23:22:54 -0300

    doc - adicionei comentários de funcionalidades nas páginas pertencentes ao infra/db (connect.php, script.sql).

--- infra/db/connect.php
+++ infra/db/connect.php
@@ -1,13 +1,15 @@
 <?php
-    session_start();
+    session_start(); // Inicia ou recupera a sessão do usuário
 
     $host = "localhost";
     $user = "root";
     $pass = "root";
     $db = "sistema_simples";
-    
+
+    // Cria a conexão com o banco de dados
     $conn = new mysqli($host,$user,$pass,$db);
 
+    // Teste da conexão
     // if($conn->connect_error){
     //     die("Erro na conexão");
     // }else{
--- infra/db/script.sql
+++ infra/db/script.sql
@@ -1,11 +1,15 @@
 CREATE DATABASE sistema_simples;
+-- Cria o banco de dados
 
 USE sistema_simples;
+-- Seleciona o banco criado
 
 CREATE TABLE usuario (
     id INT AUTO_INCREMENT PRIMARY KEY,
     usuario VARCHAR(255) NOT NULL,
     senha VARCHAR(255) NOT NULL
 );
+-- Cria a tabela de usuários
 
 INSERT INTO usuario (usuario, senha) VALUE ('admin','123');
+-- Insere um usuário inicial
